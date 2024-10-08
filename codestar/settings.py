39e999daeaa41fefd2a7a60dc7d7aaa632b1a818
Adam Green <adam@bristlebird.com>
2024-10-08 09:23:29 +0000

    prep for heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ad6bx1$2a^cyegsll2t)0b(n$uid_c^&-ah5q&a64taf9r25&d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-bristlebird-djangoblog-n4kmut46erv.ws.codeinstitute-ide.net',]
+ALLOWED_HOSTS = ['8000-bristlebird-djangoblog-n4kmut46erv.ws.codeinstitute-ide.net', '.herokuapp.com',]
 
 
 # Application definition
