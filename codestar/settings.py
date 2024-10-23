9bb5edb9e1ad14bc2ee6d1a8f100ed4f967bed0d
Adam Green <adam@bristlebird.com>
2024-10-23 22:45:13 +0000

    update post db maodel

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-bristlebird-djangoblog-n4kmut46erv.ws.codeinstitute-ide.net', '.herokuapp.com',]
 
