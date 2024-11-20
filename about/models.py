eebe18d1e8fb3dfa99cdae92fc9eaf3203e98125
Adam Green <adam@bristlebird.com>
2024-11-20 10:49:31 +0000

    add urls, about template &  update admin

--- about/models.py
+++ about/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class About(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
 
     def __str__(self):
-        return f"{self.title}"
+        return self.title
